814ce9a6113a28c6aa7c623dbf6f6b916ca08989
Johan <rrjphatomjh@gmail.com>
2026-02-14 02:56:26 -0500

    primer deploy

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Página principal
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Página carta
+@app.route("/carta")
+def carta():
+    return render_template("carta.html")
+
+# Página canción (opcional)
+@app.route("/cancion")
+def cancion():
+    return render_template("cancion.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
